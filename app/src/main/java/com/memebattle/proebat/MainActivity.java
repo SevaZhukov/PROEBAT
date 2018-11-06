830fe92b8c13643aebb24b9771a5b6e2be445093
seva <mrswimmerlab@gmail.com>
2018-11-06 22:06:06 +0700

    add menu

--- app/src/main/java/com/memebattle/proebat/MainActivity.java
+++ app/src/main/java/com/memebattle/proebat/MainActivity.java
@@ -1,8 +1,13 @@
 package com.memebattle.proebat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
